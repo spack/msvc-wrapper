695199f60af9eddbcbbc485730d35828b8517962
John Parent <john.parent@kitware.com>
2024-11-21 19:36:53 -0500

    rm debug cruft

--- src/main.cxx
+++ src/main.cxx
@@ -30,28 +30,22 @@ int main(int argc, const char* argv[]) {
     }
     else {
         // Ensure required variables are set
-        std::cout << "hi\n";
-        // try {
-        //     validate_spack_env();
-        // }
-        // catch (SpackCompilerContextException e)
-        // {
-        //     std::cerr << "Spack compiler environment not properly established, please setup the environment and try again\n";
-        //     return 99;
-        // }
+        try {
+            validate_spack_env();
+        }
+        catch (SpackCompilerContextException e)
+        {
+            std::cerr << "Spack compiler environment not properly established, please setup the environment and try again\n";
+            return 99;
+        }
         // Determine which tool we're trying to run
-        std::cout << "hello\n";
         std::unique_ptr<ToolChainInvocation> tchain(ToolChainFactory::ParseToolChain(argv));
-        std::cout << "auhweidiesn\n";
         // Establish Spack compiler/linker modifications from env
         SpackEnvState spack = SpackEnvState::loadSpackEnvState();
-        std::cout << "goodbye\n";
         // Apply modifications to toolchain invocation
         tchain->interpolateSpackEnv(spack);
-        std::cout << "adiuea\n";
         // Execute coolchain invocation
         tchain->invokeToolchain();
-        std::cout << "BORK\n";
         // Any errors caused by the run are reported via the
         // toolchain, if we reach here, we've had success, exit
     }
